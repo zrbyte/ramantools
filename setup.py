82abfa027fb96efa536addf15d10cfbad0f1a834
zrbyte <zrbyte@users.noreply.github.com>
2023-08-13 15:53:28 +0200

    update to setup.py

--- setup.py
+++ setup.py
@@ -19,6 +19,6 @@ setup(
     url = "https://github.com/zrbyte/ramantools",
     classifiers = [
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: >=3.10",
+        "Programming Language :: Python :: 3",
     ],
 )
