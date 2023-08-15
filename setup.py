c1c409bac48bbe64f84c50e405d67adbc7e31a72
zrbyte <zrbyte@users.noreply.github.com>
2023-08-15 14:08:54 +0200

    update to setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "ramantools",
-    version = "0.2.7",
+    version = "0.2.8",
     packages = find_packages(),
     # python_requires='>=3.10',
     install_requires = [
